d4381a3cc8c74f76e90005ddf4a318909461672d
Karina <kmirova731@gmail.com>
2023-07-17 14:02:25 +0600

    Upd: first Room model created, Color.kt updated, constant util added

--- app/src/main/java/com/example/todo_compose/data/models/Priority.kt
+++ app/src/main/java/com/example/todo_compose/data/models/Priority.kt
@@ -1,8 +1,14 @@
 package com.example.todo_compose.data.models
 
+import androidx.compose.ui.graphics.Color
+import com.example.todo_compose.ui.theme.HighPriorityColor
+import com.example.todo_compose.ui.theme.LowPriorityColor
+import com.example.todo_compose.ui.theme.MediumPriorityColor
+import com.example.todo_compose.ui.theme.NonePriorityColor
+
 enum class Priority(val color: Color) {
-    HIGH(),
-    MEDIUM(),
-    LOW(),
-    NONE()
+    HIGH(HighPriorityColor),
+    MEDIUM(MediumPriorityColor),
+    LOW(LowPriorityColor),
+    NONE(NonePriorityColor)
 }
--- app/src/main/java/com/example/todo_compose/data/models/ToDoTask.kt
+++ app/src/main/java/com/example/todo_compose/data/models/ToDoTask.kt
@@ -1,9 +1,15 @@
 package com.example.todo_compose.data.models
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.todo_compose.util.Constants.DATABASE_TABLE
 
 
-@Entity
+@Entity(tableName = DATABASE_TABLE)
 data class ToDoTask(
-
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val description: String,
+    val priority: Priority
 )
